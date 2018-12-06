e3fc5f8beaf9be0439f62ebe7ffd43d7308d9c68
ghozizzz <ghozi98@yahoo.com>
2018-12-06 11:13:33 +0700

    Update SQL

--- application/views_17_05_2018/layout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $this->load->view('mythemes/header');
-    $this->load->view('mythemes/menu');
-    $this->load->view($content);
-    $this->load->view('mythemes/footer');
-?>
--- application/views_25_03_2018/beranda.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-12 alert-warning alert-dismissable">        
-        <h4 style="color:navy"><i class="fa fa-home"></i> Welcome</h4>          
-    </div>
-    <div class="col-md-12 alert-warning alert-dismissable">        
-        <img src="<?php echo base_url(); ?>img/bg.png" width="100%"> 
-    </div>
-</div>
