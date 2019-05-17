ba8275c52a4471a73a506be068e32a4baaf6fe14
hendryrafdi <hendryrafdi@gmail.com>
2019-05-17 13:39:33 +0700

    resmi

--- application/helpers/target_url_helper.php
+++ application/helpers/target_url_helper.php
@@ -47,5 +47,45 @@ if ( ! function_exists('target_url'))
 		}
 		
 		return $url;
+
+		// switch ($id) {
+		// 	case 1:
+		// 		$url = 'http://tsc.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 2:
+		// 		$url = 'http://tlys.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 3:
+		// 		$url = 'http://timi.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 4:
+		// 		$url = 'http://tpkm.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 5:
+		// 		$url = 'http://tscn.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 6:
+		// 		$url = 'http://tamj.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 7:
+		// 		$url = 'http://tkrw.kmprakasa.com/index.php/';
+		// 		break;
+
+		// 	case 8:
+		// 		$url = 'http://tmtu.kmprakasa.com/index.php/';
+		// 		break;
+			
+		// 	// default:
+		// 	// 	# code...
+		// 	// 	break;
+		// }
+		
+		// return $url;
 	}
 }
