f4ced616925c9e7a45f86074dc000d6b2ed92161
Hendry <hendryrafdi@gmail.com>
2019-09-23 14:52:38 +0700

    print sj cv kmp

--- application/views/resmi/surat_jalan/print_sj_cv_kmp.php
+++ application/views/resmi/surat_jalan/print_sj_cv_kmp.php
@@ -88,16 +88,23 @@
                 ?>
                     <tr>
                         <!-- <td style="border-bottom: 1px solid; border-left: 1px solid;" align="center"><?= $no ?></td> -->
-                        <td style="border-bottom: 1px solid; border-left: 1px solid;"><?= $v->nama_item ?><br>(Ongkos Kerja)</td>
-                        <td style="border-bottom: 1px solid; border-left: 1px solid;" align="center"><?= number_format($v->total_netto,2,".",",")." ".$v->uom ?></td>
-                        <td style="border-bottom: 1px solid; border-left: 1px solid;"><?= $header['nama_customer2'] ?></td>
-                        <td style="border-bottom: 1px solid; border-left: 1px solid; border-right: 1px solid;"></td>
+                        <td style="border-left: 1px solid;"><?= $v->nama_item ?><br>(Ongkos Kerja)</td>
+                        <td style="border-left: 1px solid;" align="center"><?= number_format($v->total_netto,2,".",",")." ".$v->uom ?></td>
+                        <td style="border-left: 1px solid;"><?= $header['nama_customer2'] ?></td>
+                        <td style="border-left: 1px solid; border-right: 1px solid;"></td>
                     </tr>
                 <?php
                         $total += $v->total_netto;
                         $no++; 
                     } 
                 ?>
+                <tr>
+                    <!-- <td style="border-bottom: 1px solid; border-left: 1px solid;" align="center"><?= $no ?></td> -->
+                    <td style="border-bottom: 1px solid; border-left: 1px solid;">&nbsp;</td>
+                    <td style="border-bottom: 1px solid; border-left: 1px solid;" align="center">&nbsp;</td>
+                    <td style="border-bottom: 1px solid; border-left: 1px solid;">&nbsp;</td>
+                    <td style="border-bottom: 1px solid; border-left: 1px solid; border-right: 1px solid;">&nbsp;</td>
+                </tr>
                 <tr>
                     <td style="border-bottom: 1px solid; border-left: 1px solid;" align="right"><b></b></td>
                     <td style="border-bottom: 1px solid; border-left: 1px solid;" align="center"><b><?= number_format($total,2,".",",")." ".$v->uom ?></b></td>
@@ -108,6 +115,9 @@
         Keterangan:
         <br>
         <table border="0" cellpadding="2" cellspacing="0" width="900px">
+            <tr>
+                <td colspan="3">&nbsp;</td>
+            </tr>
             <tr>
                 <td width="40%"></td>
                 <td width="30%">DITERIMA :</td>
